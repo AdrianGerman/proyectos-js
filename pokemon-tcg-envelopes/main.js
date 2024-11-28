6f55b8b55bedc6114bb04a2a12e0335ee5a678a5
AdrianGerman <adrian.german1019@gmail.com>
2024-11-28 08:21:36 -0600

    data persistence and consistency and new styles

--- pokemon-tcg-envelopes/main.js
+++ pokemon-tcg-envelopes/main.js
@@ -8,9 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 1; i <= 5; i++) {
       const label = document.createElement("label")
       label.innerHTML = `
-          <input type="checkbox" class="${groupName}-option" />
-          ${i}
-        `
+        <input type="checkbox" class="${groupName}-option" data-index="${i}" />
+      `
       labelsContainer.appendChild(label)
     }
 
@@ -22,14 +21,50 @@ document.addEventListener("DOMContentLoaded", () => {
     container.appendChild(counterDisplay)
 
     const checkboxes = labelsContainer.querySelectorAll(`.${groupName}-option`)
-    checkboxes.forEach((checkbox) => {
-      checkbox.addEventListener("change", () => {
-        const selectedCount = Array.from(checkboxes).filter(
-          (cb) => cb.checked
-        ).length
-        counterDisplay.textContent = `Seleccionados: ${selectedCount}`
-      })
+
+    const savedState =
+      JSON.parse(localStorage.getItem(`${groupName}-state`)) || []
+    checkboxes.forEach((checkbox, index) => {
+      if (savedState.includes(index + 1)) {
+        checkbox.checked = true
+      }
+      checkbox.addEventListener("change", () =>
+        handleCheckboxChange(checkbox, index)
+      )
     })
+
+    updateCounter()
+
+    function handleCheckboxChange(checkbox, index) {
+      if (checkbox.checked) {
+        if (index > 0 && !checkboxes[index - 1].checked) {
+          checkbox.checked = false
+          return
+        }
+      } else {
+        for (let i = index + 1; i < checkboxes.length; i++) {
+          checkboxes[i].checked = false
+        }
+      }
+
+      updateCounter()
+
+      saveState()
+    }
+
+    function updateCounter() {
+      const selectedCount = Array.from(checkboxes).filter(
+        (cb) => cb.checked
+      ).length
+      counterDisplay.textContent = `Seleccionados: ${selectedCount}`
+    }
+
+    function saveState() {
+      const state = Array.from(checkboxes)
+        .map((cb, index) => (cb.checked ? index + 1 : null))
+        .filter((index) => index !== null)
+      localStorage.setItem(`${groupName}-state`, JSON.stringify(state))
+    }
   }
 
   generateCheckboxes("charizard-counter", "charizard")
