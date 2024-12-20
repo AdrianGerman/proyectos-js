55b4e21fd22488257a6bbe9189f65dee27505297
AdrianGerman <adrian.german1019@gmail.com>
2024-12-19 21:17:08 -0600

    retrieving current time

--- 07-time-calculator-web/main.js
+++ 07-time-calculator-web/main.js
@@ -1,1 +1,10 @@
-console.log("hola, desde el js")
+function updateClock() {
+  const now = new Date()
+  const hours = now.getHours().toString().padStart(2, "0")
+  const minutes = now.getMinutes().toString().padStart(2, "0")
+  const seconds = now.getSeconds().toString().padStart(2, "0")
+  document.getElementById("clock").innerText = `${hours}:${minutes}:${seconds}`
+}
+
+setInterval(updateClock, 1000)
+updateClock()
