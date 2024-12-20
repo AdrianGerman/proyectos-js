09d73fa87b338b509498b083eab901077aa3400c
AdrianGerman <adrian.german1019@gmail.com>
2024-12-20 08:17:27 -0600

    logic js

--- 07-time-calculator-web/main.js
+++ 07-time-calculator-web/main.js
@@ -16,5 +16,41 @@ function handleKeyPress(event) {
 }
 
 function calculateTime() {
-  console.log("Has realizado una conversión!")
+  const now = new Date()
+  const timeToAdd = document.getElementById("timeToAdd").value
+
+  const timeParts = timeToAdd.split(":")
+  if (timeParts.length !== 2 || isNaN(timeParts[0]) || isNaN(timeParts[1])) {
+    document.getElementById("result").innerText =
+      "Por favor, ingrese un tiempo válido en formato hh:mm."
+    return
+  }
+
+  const hoursToAdd = parseInt(timeParts[0])
+  const minutesToAdd = parseInt(timeParts[1])
+
+  if (minutesToAdd >= 60) {
+    document.getElementById("result").innerText =
+      "Por favor, ingrese un número de minutos menor a 60."
+    return
+  }
+
+  const futureTime = new Date(
+    now.getTime() + hoursToAdd * 60 * 60 * 1000 + minutesToAdd * 60 * 1000
+  )
+
+  const formaTime = (date) => {
+    let hours = date.getHours()
+    const minutes = date.getMinutes().toString().padStart(2, "0")
+    const period = hours >= 12 ? "PM" : "AM"
+    hours = hours % 12 || 12
+    return `${hours}:${minutes} ${period}`
+  }
+
+  const currentTimeFormatted = formaTime(now)
+  const futureTimeFormatted = formaTime(futureTime)
+
+  document.getElementById(
+    "result"
+  ).innerText = `Hora actual: ${currentTimeFormatted}\nHora futura: ${futureTimeFormatted}`
 }
