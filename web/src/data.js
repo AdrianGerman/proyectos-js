6eda966a08d35334353100d4350582ca1f0c14bb
AdrianGerman <adrian.german1019@gmail.com>
2024-12-22 21:18:47 -0600

    projects array

---
+++ web/src/data.js
@@ -0,0 +1,37 @@
+export const PROJECTS = [
+  {
+    slug: "01-excel-js",
+    title: "Hoja de calculo",
+    description: "Un Excel elaborado desde 0 sin dependencias y funcional."
+  },
+  {
+    slug: "02-tier-maker",
+    title: "Tier Maker",
+    description: "Arrastra y suelta para crear tu propio tier list de niveles."
+  },
+  {
+    slug: "03-paint-win-95",
+    title: "Paint",
+    description: "La experiencia clásica y el diseño retro de paint."
+  },
+  {
+    slug: "04-gif-finder",
+    title: "Buscador de GIF's",
+    description: "Busca tus gifs favoritos con palabras clave."
+  },
+  {
+    slug: "05-stack-game",
+    title: "Stack Game",
+    description: "Juego en el cual tienes que apilar las piezas verticalmente."
+  },
+  {
+    slug: "06-pokemon-tcg-envelopes",
+    title: "Contador de sobres",
+    description: "Conteo de apertura de sobres de Pokémon TCG Pocket."
+  },
+  {
+    slug: "07-time-calculator-web",
+    title: "Calculador de horas",
+    description: "Calcula la hora con sumas de la hora actual."
+  }
+]
